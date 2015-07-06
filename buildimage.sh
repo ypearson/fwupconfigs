5ff008d94c03121d3494990f6432d6c2c36fd43e
Yvan Pearson <ypearson@nklabs.com>
2015-07-06 13:19:29 -0400

    build for . files.

--- buildimage.sh
+++ buildimage.sh
@@ -5,12 +5,12 @@ set -e
 FWUP_CONFIG=fwup-gamma.conf
 FWUP=/usr/local/bin/fwup
 
-FW_PATH=output/images/sdcard.fw
-IMG_PATH=output/images/sdcard.img
+FW_PATH=sdcard.fw
+IMG_PATH=sdcard.img
 
 echo "Creating firmware file..."
-rm -f FW_PATH
-PROJECT_ROOT=$PROJECT_ROOT $FWUP -c -f $FWUP_CONFIG -o $FW_PATH
+#rm -f FW_PATH
+$FWUP -c -f $FWUP_CONFIG -o $FW_PATH
 
 echo "Creating raw SDCard image file..."
 rm -f $IMG_PATH
